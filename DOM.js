7a1aa2dc4aa9b19b423552716c0d0a4ad8e26e7f
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2022-11-22 20:26:44 -0500

    aaaa

--- DOM.js
+++ DOM.js
@@ -1,5 +1,3 @@
-
-
 const input = ()=>{return prompt("What do you want the text to be?");}
 document.getElementById("button").addEventListener("click", input);
 document.getElementById("element").innerHTML = input(); 
